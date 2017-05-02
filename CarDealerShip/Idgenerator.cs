6638a54e5e1ef21f91d68a6e836c4285bf18addd
Nikolai <niko6407@edu.easj.dk>
2017-05-02 16:32:34 +0200

    første forsøg på en nr. generator

--- CarDealerShip/Idgenerator.cs
+++ CarDealerShip/Idgenerator.cs
@@ -1,7 +1,60 @@
-﻿namespace CarDealerShip
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealerShip
 {
-    public class Idgenerator
+    public static class IdGenerator
     {
-        
+        private static Random _ID;
+        private static List<int> _IDList;
+          static IdGenerator()
+        {
+            _ID = new Random();
+            _IDList = new List<int>();
+        }
+
+        public static int GenerateIdChef()
+        {
+            int value = _ID.Next(2000, 2999);
+            for (int i = 0; i < 100; i++)
+          {
+                if (_IDList.Contains(value))
+                {
+                    value = _ID.Next(2000, 2999);
+                    i++;
+
+                }
+                else
+                {
+                 _IDList.Add(value);
+                 break;
+                }
+          }
+            
+            return value;
+        }
+
+        public static int GenerateIdSælger()
+        {
+            int value = _ID.Next(1000, 1999);
+            for (int i = 0; i < 100; i++)
+            {
+                if (_IDList.Contains(value))
+                {
+                    value = _ID.Next(1000, 1999);
+                    i++;
+
+                }
+                else
+                {
+                    _IDList.Add(value);
+                    break;
+                }
+            }
+
+            return value;
+        }
     }
 }
