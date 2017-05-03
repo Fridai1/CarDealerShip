17ecea4ac1f59cc5f958dc1a4725013807230542
Nikolai <niko6407@edu.easj.dk>
2017-05-03 10:55:49 +0200

    Fixet fremtidige problemer og tilføjet //comments.

--- CarDealerShip/Idgenerator.cs
+++ CarDealerShip/Idgenerator.cs
@@ -7,36 +7,37 @@ namespace CarDealerShip
 {
     public static class IdGenerator
     {
-        private static Random _ID;
-        private static List<int> _IDList;
+        private static Random _ID; // statisk Instance field _ID af typen Random, som er en .Net classe.
+        private static List<int> _IDList; // laver en statisk liste af typen int til at opbevarer eksisterende ID nr, for at forhindre duplications.
           static IdGenerator()
         {
-            _ID = new Random();
-            _IDList = new List<int>();
+            _ID = new Random(); // instancere Random
+            _IDList = new List<int>(); // opretter listen.
         }
 
-        public static int GenerateIdChef()
+        public static int GenerateIdChef()  // Statisk klasse for at oprette et ID for en CHEF.
         {
-            int value = _ID.Next(2000, 2999);
+            int value = _ID.Next(2000, 2999); // laver et ID der er mellem 2000-2999 og gemmer værdien i value.
             for (int i = 0; i < 1000; i++)
           {
-                if (_IDList.Contains(value))
-                {
+                if (_IDList.Contains(value)) // tjekker om den fundne værdi allerede eksistere blandt de andre ID'er 
+                {                            // eksistere den laver den et nyt ID og starter loopet forfra indtil den har fundet et som ikke allerede eksistere
                     value = _ID.Next(2000, 2999);
                     i++;
 
                 }
-                else
+                else // når den finder et ID som ikke eksistere tilføjer den, ID'et i listen og retunere det nye ID.
                 {
                  _IDList.Add(value);
+                    i = 0;  // Dette burde Resette i til 0, hvergang den finder en værdi som ikke allerede eksistere, dette er for at forhindre den statiske klasse i at kører i>1000
                  return value;
                 }
           }
             
-            return value;
+            return value; // 
         }
 
-        public static int GenerateIdSælger()
+        public static int GenerateIdSælger()  // fungere på samme måde some above.
         {
             int value = _ID.Next(1000, 1999);
             for (int i = 0; i < 1000; i++)
@@ -50,6 +51,7 @@ namespace CarDealerShip
                 else
                 {
                     _IDList.Add(value);
+                    i = 0;
                     return value;
                 }
             }
