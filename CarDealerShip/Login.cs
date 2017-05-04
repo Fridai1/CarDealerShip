da32fe22af6a626918055a79fc8a16b66df39b16
Allan Holm Søndergaard <alla2653@edu.easj.dk>
2017-05-04 21:04:10 +0200

    en oprettet klasse der mangler kød

---
+++ CarDealerShip/Login.cs
@@ -0,0 +1,27 @@
+﻿namespace CarDealerShip
+{
+    public class Login
+    {
+        private bool _succeslogin;
+
+        private bool LoginForsøg(int ID, string Password)
+        {
+            if (MedarbejderKatalog.contains(ID))
+            {
+
+                if (MedarbejderKatalog.ID.Password == Password)
+                {
+
+                    return _succeslogin = true;
+                }
+               
+            }
+
+            else
+            {
+                return _succeslogin = false;
+            }
+           
+        }
+    }
+}
