7c767c39f817feb77567e250b20fd80a888fdc14
nikol <nikol@DESKTOP-5K50T5P>
2017-05-05 12:11:28 +0200

    tilføjet bil og bil katalog

---
+++ CarDealerShip/DomainLayer/Bil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerShip
+{
+    class Bil
+    {
+        public Bil(string farve, int stelNr, bool ny, double motorStr, string type, double price, string fabrikant, string model)
+        {
+            Farve = farve;
+            StelNr = stelNr;
+            Ny = ny;
+            MotorStr = motorStr;
+            Type = type;
+            Price = price;
+            Fabrikant = fabrikant;
+            Model = model;
+        }
+
+        public string Farve;
+        public int StelNr;
+        public bool Ny;
+        public double MotorStr;
+        public string Type;
+        public double Price;
+        public string Fabrikant;
+        public string Model;
+    }
+}
---
+++ CarDealerShip/DomainLayer/BilKatalog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerShip
+{
+    static class BilKatalog
+    {
+        static Dictionary<int, Bil> Katalog = new Dictionary<int, Bil>();
+
+        static void OpretBil(Bil bil)
+        {
+            Katalog.Add(bil.StelNr, bil);
+        }
+
+        static void DeleteBil(int StelNr)
+        {
+            Katalog.Remove(StelNr);
+        }
+    }
+}
