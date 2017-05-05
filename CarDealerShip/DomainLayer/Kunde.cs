6f2a6ccfe3bf0c794fe6dfebd4889c0dad9f6fcd
Anders <Anders@ANDERS-PC>
2017-05-05 13:21:10 +0200

    tilføjet kundeklasse

---
+++ CarDealerShip/DomainLayer/Kunde.cs
@@ -0,0 +1,37 @@
+﻿namespace CarDealerShip
+{
+    class Kunde : Person
+    {
+        
+
+        public Kunde(string name, string adresse, string email, int telefon) : base(adresse, email, name, telefon)
+        {
+            
+        }
+
+        public string name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string adresse
+        {
+            get { return adresse; }
+        }
+
+        public string email
+        {
+            get { return email; }
+        }
+
+        public long telefon
+        {
+            get { return telefon; }
+        }
+        
+        
+
+
+    }
+}
