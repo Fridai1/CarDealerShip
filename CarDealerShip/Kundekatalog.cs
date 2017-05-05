cdd185883aeba2d2e073cdcff119e9794342da45
Anders <Anders@ANDERS-PC>
2017-05-05 13:25:19 +0200

    Kundekatalog tilføjet

---
+++ CarDealerShip/Kundekatalog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace CarDealerShip
+{
+    class KundeKatalog
+    {
+        private List<Kunde> _kundes;
+
+        public KundeKatalog()
+        {
+            _kundes = new List<Kunde>();
+        }
+
+        public void AddKunde(Kunde aKunde)
+        {
+            _kundes.Add(aKunde);
+        }
+
+        public Kunde GetKunde(long telefon)
+        {
+            Kunde x;
+            x = null;
+            foreach (var y in _kundes)
+            {
+                if (y.telefon == telefon) x = y;
+
+
+            }
+            return x;
+        }
+
+        public void Delete(Kunde aKunde)
+        {
+            _kundes.Remove(aKunde);
+        }
+    }
+}
