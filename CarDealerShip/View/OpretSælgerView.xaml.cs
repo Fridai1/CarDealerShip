fad6e36a4cf134ff2a5f6e6ec7c3d1a7cc632633
Fridai1 <Nikolaiviby3@gmail.com>
2017-05-17 00:30:51 +0200

    prøver at rede dette vrag.

--- CarDealerShip/View/OpretSælgerView.xaml.cs
+++ CarDealerShip/View/OpretSælgerView.xaml.cs
@@ -26,10 +26,5 @@ namespace CarDealerShip
         {
             this.InitializeComponent();
         }
-
-        private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
