177621d33e85378381ae02d6b20a073d95e2f395
Allan <allan_2233@hotmail.com>
2017-05-23 11:46:11 +0200

    salg lavet om

---
+++ CarDealerShip/ViewModel/Salg/SalgDetailsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CarDealerShip.ViewModel.Base;
+
+namespace CarDealerShip
+{
+    public class SalgDetailsViewModel : DetailsViewModelBase<Salg>
+    {
+        public SalgDetailsViewModel(Salg obj) : base(obj)
+        {
+        }
+
+        public int ID
+        {
+            get
+            {
+                return DomainObject.ID;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return DomainObject.GetName;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return DomainObject.Price;
+            }
+        }
+    }
+}
---
+++ CarDealerShip/ViewModel/Salg/ViewModelFactorySalg.cs
@@ -0,0 +1,25 @@
+﻿using CarDealerShip.ViewModel.Base;
+
+namespace CarDealerShip
+{
+    public class ViewModelFactorySalg : ViewModelFactoryBase<Salg>
+    {
+        
+        
+            public override DetailsViewModelBase<Salg> CreateDetailsViewModel(Salg obj)
+            {
+                return new SalgDetailsViewModel(obj);
+            }
+
+            public override ItemViewModelBase<Salg> CreateItemViewModel(Salg obj)
+            {
+                return new SalgItemViewModel(obj);
+            }
+
+            public override MasterViewModelBase<Salg> CreateMasterViewModel()
+            {
+                return new SalgMasterViewModel();
+            }
+
+    }
+}
