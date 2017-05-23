b28c5510029bc57eeb64797de00561ec8fe85ffb
Peter <Peter@PETERSPC>
2017-05-23 10:30:28 +0200

    Updatered Medarbjeder View klasser

--- CarDealerShip/ViewModel/Medarbejder/MedarbejderMasterViewModel.cs
+++ CarDealerShip/ViewModel/Medarbejder/MedarbejderMasterViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarDealerShip.ViewModel.Base;
 
 namespace CarDealerShip.ViewModel.Medarbejder
 {
-    class MedarbejderMasterViewModel
+    class MedarbejderMasterViewModel : MasterViewModelBase<CarDealerShip.Medarbejder>
     {
+    
     }
 }
