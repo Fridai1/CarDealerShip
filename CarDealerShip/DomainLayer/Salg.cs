015363914cc84525567fb855f73d0ee717640730
Allan <allan_2233@hotmail.com>
2017-05-24 11:00:22 +0200

    udkommenteret kode vi ikke har tænkt os at bruge i provision jeg har kommenteret på indholdet af klassen, mht hvorfor de har get og set

--- CarDealerShip/DomainLayer/Salg.cs
+++ CarDealerShip/DomainLayer/Salg.cs
@@ -8,25 +8,25 @@
          
 
 
-        public string GetName
+        public string GetName //grunden til at der både er en get og set, er fordi at dette er en klasse for sig selv, og "bare" gemmer de informationer som den får fra de andre klasser.
         {
             get { return _name; }
             set { _name = value; }
         }
 
-        public double Price //der er ingen grund til at kunne rette i prisen for en bil når salget er foretaget, derfor er der kun en get og ikk en set
+        public double Price //samme som i name
         {
             get { return _price; }
             set { _price = value; }
         }
 
-        public int ID //samme som ovenover, her skal man kun kunne hente oplysningerne ikke rette dem
+        public int ID //samme som i både name og price
         {
             get { return _id; }
             set { _price = value; }
         }
 
-        public override void SetDefaultValues()
+        public override void SetDefaultValues() //dette er de værdier som en instance af klassen har angivet når den bliver angivet
         {
             _name = "name";
             _id = 007;
